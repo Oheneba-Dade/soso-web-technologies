a2a2950b654d55b863ea80aab5d056704462055a
Oheneba Dade <ohenebadade@gmail.com>
2023-02-22 08:14:20 +0000

    fix carousel

--- index.php
+++ index.php
@@ -47,14 +47,6 @@
     <div class="container">
       <h2>Featured Products</h2>
       <div id="carouselExampleIndicators" class="carousel slide">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-            aria-current="true" aria-label="Slide 1" style="background-color: #165166;"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"
-            style="background-color: #165166;"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"
-            style="background-color: #165166;"></button>
-        </div>
         <div class="carousel-inner">
           <div class="carousel-item active">
             <img src="assets/potted_plants/long_pot.png" class="d-block carousel-img" alt="...">
