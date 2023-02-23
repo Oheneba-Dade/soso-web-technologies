6d15d3f103d541c517219439d198b415a500fb27
Oheneba Dade <ohenebadade@gmail.com>
2023-02-23 10:21:21 +0000

    restructure folders

--- signup.php
+++ signup.php
@@ -1,4 +1,5 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,64 +11,63 @@
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">
     <link rel="manifest" href="site.webmanifest">
     <title>Sign Up - Dua</title>
-
-
-
-    <link rel="stylesheet" href="signup.css">
+    <link rel="stylesheet" href="css/signup.css">
 </head>
 
 
 <body>
 
 
-<div class="container">
-    <div class = "form-container">
+    <div class="container">
+        <div class="form-container">
 
-        <div class = "header">
-        <p class="heading" id="trial">Sign Up</p>
-        
-</div>
-
-
-        <form name="form" action="process.php" method="POST" >
-            <div class = "inputs">
-
-                <div class="inputfield">
-                <input type="text" placeholder="Username" name = "uname" id="uname" required>
-                </div>
-
-                <div class="inputfield">
-                    <input type="email" placeholder="Email" name = "email" id="email" required>
-                </div>
-
-                
-                <div class="inputfield" >
-                    <input type="text" placeholder="Phone Number" name = "num"  id="num">
-                </div>
-
-                <div class="inputfield" >
-                    <input type="password" placeholder="Password" name = "pass1"  id="pass" required>
-                </div>
-
-                <div class="inputfield">
-                    <input type="password" placeholder="Confirm Password" name = "pass2" id="pass2" required>
-                </div>
-
-            
-                <div class = "btn-field">
-                    <input id="signin" type="submit" value="Submit" onclick="validateEmail(document.form.email);validatePass(document.form.password)">
-                </div>
-
-                <p class="noaccount">Already have an account? Log in
-                    <a href="login.php">here</a>
-                </p>
+            <div class="header">
+                <p class="heading" id="trial">Sign Up</p>
 
             </div>
-        </form>
-    </div>
-</div>
 
-<script type="text/javascript" src="register.js"></script>
-    
+
+            <form name="form" action="process.php" method="POST">
+                <div class="inputs">
+
+                    <div class="inputfield">
+                        <input type="text" placeholder="Username" name="uname" id="uname" required>
+                    </div>
+
+                    <div class="inputfield">
+                        <input type="email" placeholder="Email" name="email" id="email" required>
+                    </div>
+
+
+                    <div class="inputfield">
+                        <input type="text" placeholder="Phone Number" name="num" id="num">
+                    </div>
+
+                    <div class="inputfield">
+                        <input type="password" placeholder="Password" name="pass1" id="pass" required>
+                    </div>
+
+                    <div class="inputfield">
+                        <input type="password" placeholder="Confirm Password" name="pass2" id="pass2" required>
+                    </div>
+
+
+                    <div class="btn-field">
+                        <input id="signin" type="submit" value="Submit"
+                            onclick="validateEmail(document.form.email);validatePass(document.form.password)">
+                    </div>
+
+                    <p class="noaccount">Already have an account? Log in
+                        <a href="login.php">here</a>
+                    </p>
+
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <script type="text/javascript" src="register.js"></script>
+
 </body>
+
 </html>
